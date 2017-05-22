fbc67ed52dbf136fbf13e260ed2437f07b2287e4
Suraj <suraj.bvbcet@gmail.com>
2017-05-23 00:21:01 +0530

    headers

--- dsa/dsa/commonheaders.h
+++ dsa/dsa/commonheaders.h
@@ -9,5 +9,8 @@ Code written and published by Suraj U Dixit.
 #include<iostream>
 #include<time.h>
 #include<ctype.h>
-
+#include<cstdio>
+#include<fstream>
+#include<regex>
+#include<memory>
 using namespace std;
