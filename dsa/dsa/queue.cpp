d51a393cd36056e161759d9aa33e4db3e0af5350
Suraj <suraj.bvbcet@gmail.com>
2017-05-23 03:41:28 +0530

    comments_queue

--- dsa/dsa/queue.cpp
+++ dsa/dsa/queue.cpp
@@ -8,13 +8,14 @@ Code written and published by Suraj U Dixit.
 
 namespace datastructures
 {
-
+	//constructor - defines the front and rear to default values when the object is created
 	queue::queue()
 	{
 		front = 0;
 		rear = -1;
 	}
 	
+	//sets the size of the queue by creating a dynamic array
 	int queue::setSize(int size)
 	{
 		sizeOfQueue = size;
@@ -22,6 +23,7 @@ namespace datastructures
 		return 0;
 	}
 
+	//displays the queue
 	void queue::display()
 	{
 		if (front == 0 && rear == -1)
@@ -52,15 +54,18 @@ namespace datastructures
 		cout << endl;
 	}
 
+	//deletes data from front
 	int queue::deleteData()
 	{
-		if ((front == 0) && (rear == -1))
+		//queue empty condition
+		if ((front == 0) && (rear == -1)) 
 		{
 			cout << "Queue is empty" << endl;
 		}
 
 		int retr;
 		
+		//circular queue - resets the rear and front values
 		if (front == rear)
 		{
 			retr = dataArray[front];
@@ -85,8 +90,10 @@ namespace datastructures
 
 	}
 	
+	//inserts data from rear
 	int queue::insert(int data)
 	{
+		//queue full condition
 		if ((rear == sizeOfQueue - 1 && front == 0) || ( front > 0 && rear == front - 1))
 		{
 			cout << "\nQueue is full and " << data << " cannot be inserted." << endl;
@@ -94,7 +101,8 @@ namespace datastructures
 		}
 		else
 		{
-			if (rear == sizeOfQueue -  1 && front>0)
+			//resets rear and adds data
+			if (rear == sizeOfQueue -  1 && front > 0)
 			{
 				rear = 0;
 				dataArray[rear] = data;
@@ -103,6 +111,7 @@ namespace datastructures
 			}
 			else
 			{
+				//adding data into empty queue
 				if ((front == 0 && rear == -1) || (rear != front - 1))
 				{
 					dataArray[++rear] = data;
--- dsa/dsa/queue.h
+++ dsa/dsa/queue.h
@@ -15,10 +15,10 @@ namespace datastructures
 		int * dataArray;
 		int sizeOfQueue;
 	public :
-		queue();
-		int insert(int data);
-		int setSize(int size);
-		void display();
-		int deleteData();
+		queue(); //constructor - defines the front and rear to default values when the object is created
+		int insert(int data); //inserts data from rear
+		int setSize(int size); //sets the size of the queue by creating a dynamic array
+		void display(); //displays the queue
+		int deleteData();//deletes data from front
 	};
 }
